4e39a2221fba4a3abf09b5795574974f082957fe
IsraaGuda4 <esraafikry7@gmail.com>
2023-04-20 16:59:42 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,12 @@
 
 /**
  * main - func to display a statement
+ *
  * Description: it is a C programme language
+ *
  * Return: always 0 (success)
  */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
