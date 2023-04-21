015224d8a0e06027e98bc73fd40a3047ed00de74
IsraaGuda4 <esraafikry7@gmail.com>
2023-04-20 17:18:07 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,11 +6,9 @@
  * Description: it is a C programme language
  *
  * Return: always 0 (success)
- */
-
+*/
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-} 
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
