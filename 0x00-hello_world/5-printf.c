3a4ec3eab20528e081933d64b9f5d308415c7337
IsraaGuda4 <esraafikry7@gmail.com>
2023-04-20 17:24:42 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - A main function to printf
+ * Discription: A C programme to display a statement
+ * Return: Always return 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
