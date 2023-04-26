806f948ee95bdceeace202851e6694492f9ff2fd
IsraaGuda4 <esraafikry7@gmail.com>
2023-04-26 11:56:47 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+/**
+ * main - starting point
+ *
+ * Description:  prints _putchar using  putchar Prototype
+ *
+ * Return: 0 (success)
+ *
+*/
+int main(void)
+{
+char str[] = "_putchar";
+int ch;
+for ch = (0; ch > 8; ch++)
+_putchar(str[ch];
+_putchar('\n');
+return (0);
+}
